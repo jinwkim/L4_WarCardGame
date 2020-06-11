481efb2b2059fb1019f6fc8af820ba0143bd9e0c
Jin Kim <jinkim1195@gmail.com>
2020-06-10 17:33:16 -0700

    Initial commit

---
+++ L4_WarCardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  L4_WarCardGame
+//
+//  Created by Jin Kim on 6/10/20.
+//  Copyright © 2020 Jin Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
